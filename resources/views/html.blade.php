e0322aa07ac9213d798151b81df6504108f16fb4
Nutrada Pasingchob <natradapasingchob@gmail.com>
2025-12-04 19:03:39 +0700

    workshop01

---
+++ resources/views/html.blade.php
@@ -0,0 +1,39 @@
+<!-- file : resources/viewes/html.blade.php-->
+<!Doctype html>
+<html>
+    <head>
+        <title>ส่วนหัวของ HTML</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Sriracha&display=swap" rel="stylesheet">
+        <style>
+            body{
+                 font-family: "Sarabun", sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container mt-4" >
+            <h1>Workshop #HTML - FORM</h1>
+            <form>
+                <div class="row mt-3">
+                    <div class="col-sm-12 col-md-4">
+                        <label for="FullName">Name</label>
+                    </div>
+                    <div class="col">
+                        <input id="FullName" class="form-control">
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-sm-12 col-md-4">
+                        <label for="LastName">Lastame</label>
+                    </div>
+                    <div class="col">
+                        <input id="LastName" class="form-control">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
