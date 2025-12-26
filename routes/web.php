80f66f390a7ba01832dce2b3cfbf25cd96b8ba1c
NutradaPC <natradapasingchob@gmail.com>
2025-12-26 16:43:51 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MyController; 
+use App\Http\Controllers\MyController;
+use League\Uri\Http;
 
 Route::get('/', function () {
     return view('html');
@@ -14,3 +15,5 @@ Route::get('/se', function(){
 
 Route::get('/workshop', [MyController::class, 'index']); // เปิดหน้าฟอร์ม
 Route::post('/workshop-store', [MyController::class, 'store'])->name('workshop.store'); // รับค่า
+
+Route::resource('/flights', App\Http\Controllers\FlightController::class);
